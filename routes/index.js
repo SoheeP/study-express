786aac9896c85520769c7de7aa58a904940115be
soheeP <psh950502@naver.com>
2019-09-22 16:46:46 +0900

    20190922 영화 사이트 데이터 통신 성공

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var {
+  dummy,
+  log
+} = require('./common');
+var {
+  _,
+  moment,
+  axios
+} = require('./npm_modules');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+router.get('/:category/list/:page', function (req, res, next) {
+  console.log(req.params);
+  console.log(req.params.category);
+
+  let body = dummy.listPage(req.params.category);
+  let pageName = '';
+
+  if (req.params.category === 'movie') {
+    pageName = 'movie';
+
+    axios.get(`https://yts.tl/api/v2/list_movies.json?limit=10`)
+      .then(function (response) {
+        let data = response.data;
+
+        if(data.status === 'ok'){
+          log(data.data);
+          body.movies = data.data.movies;
+          body.pageName = pageName;
+          res.render('Pages/Category/list', body);
+        }
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+  if (req.params.category === 'concert') {
+    pageName = 'concert'
+
+  }
+  if (req.params.category === 'expo') {
+    pageName = 'exhibition'
+
+  }
+
+});
+
+
 
 module.exports = router;
---
+++ routes/npm_modules.js
@@ -0,0 +1,10 @@
+var axios = require('axios');
+var moment = require('moment');
+var lodash = require('lodash');
+const util = require('util')
+
+
+exports.axios = axios;
+exports.moment = moment;
+exports._ = lodash;
+exports.util = util;
