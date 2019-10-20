0601edf22d55d56b3d7247be09c7ec5eac1cc1bc
phantom05 <monster2jy@gmail.com>
2019-10-21 02:05:13 +0900

    이메일 회원 인증, 자동가입방지 문자, 이메일 중복확인 추가

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var router = express.Router();
-var {
-  Axios
+const express = require('express');
+const router = express.Router();
+const {
+  Axios,
+  createSvgCaptcha,
+  replaceAll,
+  varifivationMailForm
 } = require('./common');
-var {
+const {
   wrap
 } = require('./middlewares');
 
-
+const {
+  nodemailer,
+  uuidv4
+} = require('./npm_modules');
 
 
 
@@ -16,7 +22,9 @@ var {
  */
 router.route('/login')
   .get(wrap((req, res, next) => {
-    res.render('Pages/Auth/login', { title: 'login' });
+    res.render('Pages/Auth/login', {
+      title: 'login'
+    });
   }))
   .post(wrap((req, res, next) => {
     console.log(req.body.email);
@@ -80,42 +88,132 @@ router.route('/logout')
  */
 router.route('/signup')
   .get(wrap(async (req, res, next) => {
-    res.render('Pages/Auth/signup', {
-      title: 'Express'
-    });
+
+
+
+
+    let captcha = createSvgCaptcha();
+    let body = {};
+    body.captcha = captcha;
+    req.session.captcha = captcha.text;
+    res.render('Pages/Auth/signup', body);
   }))
   .post(wrap(async (req, res, next) => {
     let email = req.body.email;
-    let verification = req.body.verification;
+    let emailVerification = req.body.emailVerification;
     let password = req.body.password;
     let username = req.body.username;
     let phone = req.body.phone;
+    let verifyNumber = req.body.captcha;
 
-
-    const signupConfig = {
-      url: '/auth/signup',
-      method: 'post',
-      data: {
-        email: email,
-        verification: 1,
-        password: password,
-        username: username,
-        phone: phone
-      }
-    };
-    Axios(signupConfig, function (response) {
-      let { data } = response;
-      console.log(data);
-      if(data.result === 1){
-        res.json({result: 1});
-      } else if(data.result === 4){
-        res.json({ result: 4 });
+    if (req.session.captcha === verifyNumber) {
+      if(req.session.token === emailVerification){
+        const signupConfig = {
+          url: '/auth/signup',
+          method: 'post',
+          data: {
+            email: email,
+            verification: emailVerification,
+            password: password,
+            username: username,
+            phone: phone,
+          }
+        };
+        Axios(signupConfig, function (response) {
+          let { data } = response;
+          console.log(data);
+          if (data.result === 1) {
+            res.json({ result: 1 });
+          } else if (data.result === 4) {
+            res.json({ result: 4 });
+          } else {
+            res.json({ result: 2 });
+          }
+        })
       } else {
-        res.json({result: 2});
+        res.json({result: 8})
       }
-    })
+    } else {
+      res.json({ result: 9 });
+    }
+  }))
+
+/**
+ * ROUTER: new create Captcha
+ */
+router.route('/captcha')
+  .get(wrap(async (req, res, next) => {
+    var captcha = createSvgCaptcha();
+    let body = {};
+    body.captcha = captcha;
+    req.session.captcha = captcha.text;
+    res.json(body)
   }))
 
 
+  /**
+   * ROUTER: Email verify
+   */
+router.route('/email/verify')
+.get(wrap(async (req, res, next)=>{
+  console.log(req.query);
+
+  const { email } = req.query;
+  // /email/check
+  // email
+  let checkEmailConfig = {
+    url: '/auth/email/check',
+    method: 'post',
+    data: { email }
+  }
+  Axios(checkEmailConfig).then((response) => {
+    let { data } = response;
+    // email체크값이 있을때(= 1은 아이디가 있다)
+    if(data.result !== 1){
+      //아이디가 없을때
+      res.json({ result: 1 })
+    } else {
+      //아이디가 있을때
+      res.json({ result: 2 })
+    }
+  })
+
+  // console.log(req);
+  
+}))
+  .post(wrap(async (req, res, next) => {
+    let body = {};
+    let email = req.body.email;
+    let token = uuidv4();
+    let convertToken = replaceAll(token, "-", "")
+    console.log(convertToken);
+    req.session.token = convertToken;
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'contact.culturefree@gmail.com', // generated ethereal user
+        pass: 'phantom05+' // generated ethereal password
+      }
+    });
+
+    let info = await transporter.sendMail({
+      service: 'gmail',
+      from: '"Culture Free"', // sender address
+      to: email, // list of receivers
+      subject: 'Email verification ✔', // Subject line
+      html: varifivationMailForm(convertToken) // html body
+    }, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    res.json(body);
+  }))
+
 
 module.exports = router;
+
+
