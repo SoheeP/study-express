48f5144dff6fe9b2717a99ae672f98550e8daa8e
phantom05 <monster2jy@gmail.com>
2019-10-21 02:04:34 +0900

    uuid, svg-captca, nodemailer 추가

--- routes/npm_modules.js
+++ routes/npm_modules.js
@@ -1,10 +1,16 @@
-var axios = require('axios');
-var moment = require('moment');
-var lodash = require('lodash');
-const util = require('util')
+const axios = require('axios');
+const moment = require('moment');
+const lodash = require('lodash');
+const util = require('util');
+const svgCaptcha = require('svg-captcha');
+const nodemailer = require('nodemailer');
+const uuidv4 = require('uuid/v4');
 
 
 exports.axios = axios;
 exports.moment = moment;
 exports._ = lodash;
 exports.util = util;
+exports.svgCaptcha = svgCaptcha;
+exports.nodemailer = nodemailer;
+exports.uuidv4 = uuidv4;
