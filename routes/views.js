e15382ad3ec5214773fba477d0c0e10ca09bff28
phantom05 <monster2jy@gmail.com>
2019-10-21 02:05:25 +0900

    FAQ 라우터 추가

--- routes/views.js
+++ routes/views.js
@@ -7,4 +7,8 @@ router.get('/about', function(req, res, next) {
   res.render('Pages/ViewPages/about', { pageName: 'About' });
 });
 
+
+router.get('/faq', function(req, res, next) {
+  res.render('Pages/ViewPages/faq', { pageName: 'FAQ' });
+});
 module.exports = router;
